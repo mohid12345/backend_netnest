02f67aabb23f42dbb4997358d94d7adab5dddb2e
Mohid Mohan <mohidmohan8482@gmail.com>
2024-07-13 21:04:57 +0530

    first commit

---
+++ src/models/user/userModel.ts
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from "mongoose";
+import user, {Gender} from './userTypes'
+
+const userSchema: Schema = new Schema<user>({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      name: {
+        type: String,
+        default: 'zap_user',
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      phone: {
+        type: String,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      profileImg: {
+        type: String,
+        default: 'https://img.freepik.com/premium-vector/people-saving-money_24908-51569.jpg?w=740',
+        // default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm0I8gC3EZZ894dIRJPjTYIcu-nRhxf_0C9A&s',
+      },
+      bio: {
+        type: String,
+        default: 'Discovering moments, one post at a time.'
+      },
+      gender: {
+        type: String,
+        enum: Object.values(Gender)
+      },
+}, {timestamps: true})
+
+const User = mongoose.model<user>('User', userSchema)
+export default User
