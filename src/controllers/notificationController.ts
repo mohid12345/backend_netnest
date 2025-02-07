c7b0829a01c3df4bd25c29974f63da1c13fca1e2
Mohid Mohan <mohidmohan8482@gmail.com>
2025-02-07 15:17:09 +0530

    image also populated and send

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import asyncHandler from "express-async-handler";
 import Notification from '../models/notifications/notificationModel';
-import Connections from '../models/connections/connectionModel';
 import { StatusCodes } from 'http-status-codes';
 
 
@@ -9,14 +8,12 @@ export const getNotifications = asyncHandler(
   async(req: Request, res: Response):Promise<void> => {
     try {
       const userId = req.body.userId
-      // console.log("userId", userId);
       const notifications = await Notification.find({receiverId: userId})
       .populate({
         path: 'senderId',
         select: 'userName name profileImg',
       })
       .sort({createdAt: -1})
-      // console.log("notifications", notifications);
       res.status(StatusCodes.OK).json({ notifications: notifications })
     } catch (error) {
       console.error('Error fetching notifications:', error);
