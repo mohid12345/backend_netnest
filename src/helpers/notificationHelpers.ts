6682f12e63661b6df1239d8956182a31c68112cc
Mohid Mohan <mohidmohan8482@gmail.com>
2025-02-07 15:17:09 +0530

    image also populated and send

--- src/helpers/notificationHelpers.ts
+++ src/helpers/notificationHelpers.ts
@@ -3,28 +3,19 @@ import Notification from "../models/notifications/notificationModel";
 import { NotificationInterface } from "../models/notifications/notificationTypes";
 import { sendNotification } from "../utils/socket/notificationSocket";
 
-
 interface NotificationArgs {
-  senderId: Types.ObjectId|undefined|any;
-  receiverId: Types.ObjectId|undefined|any;
-  message: string|any;
-  link: string|any;
-  postId?: Types.ObjectId|undefined|any;
+  senderId: Types.ObjectId | undefined | any;
+  receiverId: Types.ObjectId | undefined | any;
+  message: string | any;
+  link: string | any;
+  postId?: Types.ObjectId | undefined | any;
   read?: boolean;
   isDeleted?: boolean;
 }
 
-export const createNotification = async(args: NotificationArgs) : Promise<NotificationInterface> => {
+export const createNotification = async (args: NotificationArgs): Promise<NotificationInterface> => {
   try {
-    const {
-      senderId,
-      receiverId,
-      message,
-      link,
-      postId,
-      read,
-      isDeleted,
-    } = args
+    const { senderId, receiverId, message, link, postId, read, isDeleted } = args;
 
     const newNotification = new Notification({
       senderId,
@@ -34,14 +25,83 @@ export const createNotification = async(args: NotificationArgs) : Promise<Notifi
       postId,
       read,
       isDeleted,
-    })
+    });
+
+    // Save the notification
+    const savedNotification = await newNotification.save();
+
+    // Populate senderId details for  image been shown in new ws.io notification 
+    const populatedNotification = await savedNotification.populate({
+      path: "senderId",
+      select: "userName name profileImg",
+    });
+
+    // Send notification with populated user details
+    sendNotification(receiverId.toString(), populatedNotification);
+
+    return populatedNotification;
+  } catch (error) {
+    throw new Error("Error creating notification");
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Types } from "mongoose";
+// import Notification from "../models/notifications/notificationModel";
+// import { NotificationInterface } from "../models/notifications/notificationTypes";
+// import { sendNotification } from "../utils/socket/notificationSocket";
+
+
+// interface NotificationArgs {
+//   senderId: Types.ObjectId|undefined|any;
+//   receiverId: Types.ObjectId|undefined|any;
+//   message: string|any;
+//   link: string|any;
+//   postId?: Types.ObjectId|undefined|any;
+//   read?: boolean;
+//   isDeleted?: boolean;
+// }
+
+// export const createNotification = async(args: NotificationArgs) : Promise<NotificationInterface> => {
+//   try {
+//     const {
+//       senderId,
+//       receiverId,
+//       message,
+//       link,
+//       postId,
+//       read,
+//       isDeleted,
+//     } = args
+
+//     const newNotification = new Notification({
+//       senderId,
+//       receiverId,
+//       message,
+//       link,
+//       postId,
+//       read,
+//       isDeleted,
+//     })
    
     
 
-    const savedNotification = await newNotification.save()
-    sendNotification(receiverId.toString(), savedNotification);
-    return savedNotification
-  } catch (error) {
-    throw new Error('Error creating notification');
-  }
-}
+//     const savedNotification = await newNotification.save()
+//     sendNotification(receiverId.toString(), savedNotification);
+//     return savedNotification
+//   } catch (error) {
+//     throw new Error('Error creating notification');
+//   }
+// }
